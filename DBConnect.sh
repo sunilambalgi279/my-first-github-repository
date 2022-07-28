5e7563ae14e781798a970f05116512bb4d4c859b
sunilambalgi279 <sunilambalgi279@gmail.com>
2022-07-28 12:44:28 +0530

    Updated the file

--- DBConnect.sh
+++ DBConnect.sh
@@ -1,1 +1,4 @@
 THis is my first github repo, lets get started
+Git is a Distributed Version Control System and Github is a Repository Hosted service
+In simpler term Git keeps the track of each file changes in local repository
+
